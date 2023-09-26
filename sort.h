4a32d8a2f0286c0c7a09919d5253ce1d6ff320a5
JoanWaithira <joanwaithira.jw@gmail.com>
2023-09-26 04:29:27 -0700

    Shell Sort

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void shell_sort(int *array, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
